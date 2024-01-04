54ccd243eee75a8cf8400f40a68c5facd3e6de59
vanessawilliams <vanessawilliams@localhost>
2024-01-03 20:55:33 -0500

    initial commit

---
+++ src/b/DemoB.java
@@ -0,0 +1,14 @@
+package b;
+
+public class DemoB {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Tuesday");
+		
+		System.out.println("day 1");
+		System.out.println("day 2");
+		System.out.println("day 3");
+	}
+
+}
